75193ab29a86f7655e6803242c39b53c940ecb63
pineapple74 <122548032+pineapple74@users.noreply.github.com>
2023-01-13 11:26:05 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,148 @@
+input.onButtonPressed(Button.A, function () {
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . .
+        # . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # # . . .
+        # # . . .
+        # . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # # # . .
+        # # # . .
+        . # . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        # # . . .
+        # # # # .
+        # # # # .
+        # . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # # . .
+        # # # # #
+        # # # # #
+        . # . # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . # # # #
+        . # # # #
+        # . # . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . # #
+        . . # # #
+        . . # # #
+        # # . # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        # . . # #
+        # . . # #
+        . # # . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        # . . . #
+        # # . . #
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        # . . . .
+        # # . . .
+        # # # . .
+        . . # # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        # # . . .
+        # # # . .
+        # # # # .
+        . . . # #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . # # . .
+        # # # # .
+        . # # # #
+        . . . . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . # # .
+        . # # # #
+        . . # # #
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . # #
+        . . # # #
+        . . . # #
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . #
+        . . . # #
+        . . . . #
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+basic.showString("Welcome! ")
+basic.showString("Press A")
